4a092079f7885b79f5f9195560831d32f39349f9
Amin <mamp2007.mohammadi@gmail.com>
2025-09-07 10:22:38 +0330

    make a run file to see the result of project

---
+++ run.py
@@ -0,0 +1,11 @@
+"""to execute and see the programs result run this file with:
+'python run.py'"""
+from encoding import *
+opperation = None
+while opperation != "e" and opperation != "d":
+    opperation = input("if you want to encode press 'e' and if you want to decode press 'd': (e/d)").strip()
+
+if opperation == "e":
+    pass
+else:
+    pass
